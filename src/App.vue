808d3a2f5eb034a08cd7092efa061684320c8e10
GOMEZ RUIZ Ariadna <ariadna.gomez@soprasteria.com>
2020-04-04 12:25:53 +0200

    fix: style

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,23 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
     HelloWorld
   }
-};
+}
 </script>
 
 <style>
 #app {
+  margin-top: 60px;
+  color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -104,11 +104,11 @@
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
     msg: String
   }
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -116,14 +116,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
-  list-style-type: none;
   padding: 0;
+  list-style-type: none;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
